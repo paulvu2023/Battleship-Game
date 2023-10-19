08d2ca9ae4bc9c45210ea3fb55b80ce2ce61ac1d
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-10-19 14:21:03 -0700

    Add test for 'Ship' class

---
+++ src/game.test.js
@@ -0,0 +1,11 @@
+const game = require("./game");
+
+describe("Ship", () => {
+  it("sink when the # of times it has been hit = length", () => {
+    const ship = new Ship(3);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
+});
